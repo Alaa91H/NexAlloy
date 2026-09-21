2c9a71089ab9555ff0367448ea24cc149324f720
Alaa <alahus2591@gmail.com>
2026-09-22 01:56:45 +0200

    feat: add Twitch support

---
+++ app/src/main/java/io/github/nexalloy/revanced/twitch/TwitchHook.kt
@@ -0,0 +1,9 @@
+package io.github.nexalloy.revanced.twitch
+
+import io.github.nexalloy.revanced.twitch.ad.audio.BlockAudioAds
+import io.github.nexalloy.revanced.twitch.ad.video.BlockVideoAds
+
+val TwitchPatches = arrayOf(
+    BlockAudioAds,
+    BlockVideoAds,
+)
