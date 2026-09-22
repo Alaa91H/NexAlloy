ad18da8fa730d950d6a4871d6ff47c94e7493b46
Alaa <alahus2591@gmail.com>
2026-09-22 02:45:24 +0200

    feat: add Truecaller privacy runtime patches

---
+++ app/src/main/java/io/github/nexalloy/revanced/truecaller/misc/PrivacyAndUpdatePatches.kt
@@ -0,0 +1,21 @@
+package io.github.nexalloy.revanced.truecaller.misc
+
+import de.robv.android.xposed.XC_MethodReplacement
+import io.github.nexalloy.patch
+
+val DisableAppStartTelemetry = patch(
+    name = "Disable Truecaller app-start telemetry",
+    description = "Prevents AppStartTracker from enabling technical lifecycle telemetry.",
+) {
+    ::appStartTrackerFingerprint.memberOrNull
+        ?.hookMethod(XC_MethodReplacement.DO_NOTHING)
+}
+
+val DisableUpdateCheck = patch(
+    name = "Disable Truecaller update nag",
+    description = "Disables the in-app update trigger. This does not block Play Store updates.",
+    use = false,
+) {
+    ::shouldTriggerUpdateFingerprint.memberOrNull
+        ?.hookMethod(XC_MethodReplacement.returnConstant(false))
+}
