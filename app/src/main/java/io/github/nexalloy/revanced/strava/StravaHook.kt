d1cbd01d5b78224dbc45c7c04eea7f1751d3e52f
Alaa <alahus2591@gmail.com>
2026-09-22 08:50:16 +0200

    feat: expose screen capture bypass for existing apps

--- app/src/main/java/io/github/nexalloy/revanced/strava/StravaHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/strava/StravaHook.kt
@@ -1,9 +1,11 @@
 package io.github.nexalloy.revanced.strava
 
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 import io.github.nexalloy.revanced.strava.subscription.UnlockSubscription
 import io.github.nexalloy.revanced.strava.upselling.DisableSubscriptionSuggestions
 
 val StravaPatches = arrayOf(
+    AllowScreenCapture,
     UnlockSubscription,
-    DisableSubscriptionSuggestions
+    DisableSubscriptionSuggestions,
 )
