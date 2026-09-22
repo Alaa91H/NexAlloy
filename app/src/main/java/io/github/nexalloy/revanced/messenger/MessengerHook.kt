15e8fe7f08e05f08d9885fbb9f8c658598bd096b
Alaa <alahus2591@gmail.com>
2026-09-22 08:49:26 +0200

    feat: expose screen capture bypass for existing apps

--- app/src/main/java/io/github/nexalloy/revanced/messenger/MessengerHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/messenger/MessengerHook.kt
@@ -1,7 +1,9 @@
 package io.github.nexalloy.revanced.messenger
 
 import io.github.nexalloy.revanced.messenger.ads.HideInboxAds
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val MessengerPatches = arrayOf(
+    AllowScreenCapture,
     HideInboxAds,
 )
