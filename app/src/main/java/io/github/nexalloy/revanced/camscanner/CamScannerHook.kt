6fe40dc46487de4d628a3822184beabcd583e378
Alaa <alahus2591@gmail.com>
2026-09-22 03:33:39 +0200

    feat: allow optional screen capture restriction bypass

--- app/src/main/java/io/github/nexalloy/revanced/camscanner/CamScannerHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/camscanner/CamScannerHook.kt
@@ -4,7 +4,10 @@ import io.github.nexalloy.revanced.camscanner.telemetry.DisableTelemetry
 import io.github.nexalloy.revanced.shared.privacy.BlockCommonDisplayAds
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
+
 val CamScannerPatches = arrayOf(
+    AllowScreenCapture,
     DisableTelemetry,
     DisableCommonAnalytics,
     BlockCommonDisplayAds,
