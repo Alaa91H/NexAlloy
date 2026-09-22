9d1b29808a2a64bd54448e61bb89c1ed5cb1c997
Alaa <alahus2591@gmail.com>
2026-09-22 08:58:59 +0200

    feat: wire expanded Reddit X and Meta patches

--- app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
@@ -1,5 +1,9 @@
 package io.github.nexalloy.revanced.meta
 
 import io.github.nexalloy.revanced.meta.ads.HideAds
+import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
 
-val MetaPatches = arrayOf(HideAds)
+val MetaPatches = arrayOf(
+    HideAds,
+    SanitizeMetaSharingLinks,
+)
