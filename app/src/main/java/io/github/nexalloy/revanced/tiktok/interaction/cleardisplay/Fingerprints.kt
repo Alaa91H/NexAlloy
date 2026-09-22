8186326434081e07c0119e0413472280eaffc520
Alaa <alahus2591@gmail.com>
2026-09-22 09:36:23 +0200

    feat(tiktok): remember clear display state

---
+++ app/src/main/java/io/github/nexalloy/revanced/tiktok/interaction/cleardisplay/Fingerprints.kt
@@ -0,0 +1,23 @@
+package io.github.nexalloy.revanced.tiktok.interaction.cleardisplay
+
+import io.github.nexalloy.morphe.fingerprint
+import org.luckypray.dexkit.query.enums.StringMatchType
+
+val onClearDisplayEventFingerprint = fingerprint {
+    methodMatcher { name = "onClearModeEvent" }
+    classMatcher {
+        className("/ClearModePanelComponent", StringMatchType.EndsWith)
+    }
+}
+
+val onRenderFirstFrameBodyFingerprint = fingerprint {
+    returns("V")
+    parameters(
+        "Lcom/ss/android/ugc/aweme/feed/controller/PlayerController;",
+        "LX/0pb0;",
+    )
+    methodMatcher { name = "LLILZIL" }
+    classMatcher {
+        className("/feed/controller/PlayerController", StringMatchType.EndsWith)
+    }
+}
