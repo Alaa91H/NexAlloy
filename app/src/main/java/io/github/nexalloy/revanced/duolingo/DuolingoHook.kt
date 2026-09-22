bf940d5c6c72a0e05c96030c9cb94146b7c50f85
Alaa <alahus2591@gmail.com>
2026-09-22 03:33:33 +0200

    feat: allow optional screen capture restriction bypass

--- app/src/main/java/io/github/nexalloy/revanced/duolingo/DuolingoHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/duolingo/DuolingoHook.kt
@@ -4,7 +4,10 @@ import io.github.nexalloy.revanced.duolingo.ads.DisableAds
 import io.github.nexalloy.revanced.duolingo.icon.DisableDynamicIcon
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
+
 val DuolingoPatches = arrayOf(
+    AllowScreenCapture,
     DisableAds,
     DisableDynamicIcon,
     DisableCommonAnalytics,
