703e0b304dd35c4d6f50a1ac84d2ca3033300408
Alaa <alahus2591@gmail.com>
2026-09-22 02:25:07 +0200

    feat: register TikTok playback speed patch

--- app/src/main/java/io/github/nexalloy/revanced/tiktok/TikTokHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/tiktok/TikTokHook.kt
@@ -3,12 +3,14 @@ package io.github.nexalloy.revanced.tiktok
 import io.github.nexalloy.revanced.tiktok.ad.HideAds
 import io.github.nexalloy.revanced.tiktok.interaction.downloads.EnableDownloads
 import io.github.nexalloy.revanced.tiktok.interaction.seekbar.ShowSeekbar
+import io.github.nexalloy.revanced.tiktok.interaction.speed.EnablePlaybackSpeed
 import io.github.nexalloy.revanced.tiktok.misc.login.DisableLoginRequirement
 import io.github.nexalloy.revanced.tiktok.misc.login.FixGoogleLogin
 
 val TikTokPatches = arrayOf(
     HideAds,
     ShowSeekbar,
+    EnablePlaybackSpeed,
     EnableDownloads,
     DisableLoginRequirement,
     FixGoogleLogin,
