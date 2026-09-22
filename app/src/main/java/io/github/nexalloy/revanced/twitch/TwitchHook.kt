ff6c118eb41062cfd29ccdd7544e46c41c694d81
Alaa <alahus2591@gmail.com>
2026-09-22 09:08:00 +0200

    feat: wire Twitch and TikTok expansion patches

--- app/src/main/java/io/github/nexalloy/revanced/twitch/TwitchHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/twitch/TwitchHook.kt
@@ -1,14 +1,18 @@
 package io.github.nexalloy.revanced.twitch
 
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 import io.github.nexalloy.revanced.twitch.ad.audio.BlockAudioAds
+import io.github.nexalloy.revanced.twitch.ad.display.HideDisplayAds
 import io.github.nexalloy.revanced.twitch.ad.video.BlockVideoAds
 import io.github.nexalloy.revanced.twitch.chat.antidelete.ShowDeletedMessages
 import io.github.nexalloy.revanced.twitch.chat.autoclaim.AutoClaimChannelPoints
 import io.github.nexalloy.revanced.twitch.debug.EnableDebugMode
 
 val TwitchPatches = arrayOf(
+    DisableCommonAnalytics,
     BlockAudioAds,
     BlockVideoAds,
+    HideDisplayAds,
     AutoClaimChannelPoints,
     ShowDeletedMessages,
     EnableDebugMode,
