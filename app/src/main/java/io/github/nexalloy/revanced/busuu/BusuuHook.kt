75dd8ee7b3bb5156cc24d935a765dfa3efa92ec5
Alaa <alahus2591@gmail.com>
2026-09-22 03:33:36 +0200

    feat: allow optional screen capture restriction bypass

--- app/src/main/java/io/github/nexalloy/revanced/busuu/BusuuHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/busuu/BusuuHook.kt
@@ -3,7 +3,10 @@ package io.github.nexalloy.revanced.busuu
 import io.github.nexalloy.revanced.shared.privacy.BlockCommonDisplayAds
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
+
 val BusuuPatches = arrayOf(
+    AllowScreenCapture,
     DisableCommonAnalytics,
     BlockCommonDisplayAds,
 )
