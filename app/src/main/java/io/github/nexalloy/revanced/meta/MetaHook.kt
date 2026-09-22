4aa3be1cd1839dd3fa8497f4b5e5a4b694b193ae
Alaa <alahus2591@gmail.com>
2026-09-22 09:01:36 +0200

    feat: add optional common analytics suppression to social apps

--- app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
@@ -3,7 +3,10 @@ package io.github.nexalloy.revanced.meta
 import io.github.nexalloy.revanced.meta.ads.HideAds
 import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
 
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
+
 val MetaPatches = arrayOf(
+    DisableCommonAnalytics,
     HideAds,
     SanitizeMetaSharingLinks,
 )
