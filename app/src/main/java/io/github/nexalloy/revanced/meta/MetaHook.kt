f4834c926e0ce8928abef55c77d16b811d7c93d4
Alaa <alahus2591@gmail.com>
2026-09-22 09:35:14 +0200

    feat: expand privacy and ad controls for existing apps

--- app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
@@ -1,5 +1,11 @@
 package io.github.nexalloy.revanced.meta
 
 import io.github.nexalloy.revanced.meta.ads.HideAds
+import io.github.nexalloy.revanced.shared.privacy.BlockCommonDisplayAds
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
-val MetaPatches = arrayOf(HideAds)
+val MetaPatches = arrayOf(
+    HideAds,
+    DisableCommonAnalytics,
+    BlockCommonDisplayAds,
+)
