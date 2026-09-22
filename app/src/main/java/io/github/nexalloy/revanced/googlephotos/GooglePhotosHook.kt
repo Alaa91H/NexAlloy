3a0fd1999afd0f9e7fd53eba19a44f08b84e1103
Alaa <alahus2591@gmail.com>
2026-09-22 08:49:16 +0200

    feat: expose screen capture bypass for existing apps

--- app/src/main/java/io/github/nexalloy/revanced/googlephotos/GooglePhotosHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/googlephotos/GooglePhotosHook.kt
@@ -2,8 +2,10 @@ package io.github.nexalloy.revanced.googlephotos
 
 import io.github.nexalloy.revanced.googlephotos.misc.backup.EnableDCIMFoldersBackupControl
 import io.github.nexalloy.revanced.googlephotos.misc.features.SpoofFeaturesPatch
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val GooglePhotosPatches = arrayOf(
+    AllowScreenCapture,
     SpoofFeaturesPatch,
     EnableDCIMFoldersBackupControl,
 )
