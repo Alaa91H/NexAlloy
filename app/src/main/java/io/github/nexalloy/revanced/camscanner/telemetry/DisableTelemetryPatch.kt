679cf9cd0cfda2cc7f2ba0ed1e3996f234ccd48c
Alaa <alahus2591@gmail.com>
2026-09-22 02:44:40 +0200

    feat: add camscanner telemetry support

---
+++ app/src/main/java/io/github/nexalloy/revanced/camscanner/telemetry/DisableTelemetryPatch.kt
@@ -0,0 +1,16 @@
+package io.github.nexalloy.revanced.camscanner.telemetry
+
+import de.robv.android.xposed.XC_MethodReplacement
+import io.github.nexalloy.patch
+
+val DisableTelemetry = patch(
+    name = "Disable CamScanner telemetry",
+    description = "Disables CamScanner's custom LogAgent telemetry pipeline.",
+) {
+    ::isSkipLoggingFingerprint.memberOrNull
+        ?.hookMethod(XC_MethodReplacement.returnConstant(true))
+
+    ::logAgentRecordFingerprints.dexMethodList.forEach { method ->
+        method.hookMethod(XC_MethodReplacement.DO_NOTHING)
+    }
+}
