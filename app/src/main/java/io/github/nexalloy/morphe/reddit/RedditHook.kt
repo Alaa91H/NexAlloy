96dae06064f85d7660dd09da552730c00d9a9d1e
Alaa <alahus2591@gmail.com>
2026-09-22 09:02:22 +0200

    style: group social privacy imports

--- app/src/main/java/io/github/nexalloy/morphe/reddit/RedditHook.kt
+++ app/src/main/java/io/github/nexalloy/morphe/reddit/RedditHook.kt
@@ -5,7 +5,6 @@ import io.github.nexalloy.morphe.reddit.layout.subredditdialog.RemoveNsfwWarning
 import io.github.nexalloy.morphe.reddit.misc.openlink.OpenLinksDirectly
 import io.github.nexalloy.morphe.reddit.misc.openlink.OpenLinksExternally
 import io.github.nexalloy.morphe.reddit.misc.privacy.SanitizeSharingLinks
-
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
 val RedditPatches = arrayOf(
