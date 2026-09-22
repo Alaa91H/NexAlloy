3f1c75367d58bd14e1daeb985b09898f179e5251
Alaa <alahus2591@gmail.com>
2026-09-22 09:33:50 +0200

    feat(tiktok): register modern interaction and privacy patches

--- app/src/main/java/io/github/nexalloy/revanced/tiktok/TikTokHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/tiktok/TikTokHook.kt
@@ -1,11 +1,18 @@
 package io.github.nexalloy.revanced.tiktok
 
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 import io.github.nexalloy.revanced.tiktok.ad.HideAds
 import io.github.nexalloy.revanced.tiktok.interaction.downloads.EnableDownloads
+import io.github.nexalloy.revanced.tiktok.interaction.looping.StopVideoLooping
+import io.github.nexalloy.revanced.tiktok.interaction.quickactions.DisableLongPressQuickShare
+import io.github.nexalloy.revanced.tiktok.interaction.quickactions.DisableLongPressRepost
+import io.github.nexalloy.revanced.tiktok.interaction.quickactions.HideQuickCommentReactions
 import io.github.nexalloy.revanced.tiktok.interaction.seekbar.ShowSeekbar
 import io.github.nexalloy.revanced.tiktok.interaction.speed.EnablePlaybackSpeed
+import io.github.nexalloy.revanced.tiktok.misc.externalbrowser.OpenExternalLinks
 import io.github.nexalloy.revanced.tiktok.misc.login.DisableLoginRequirement
 import io.github.nexalloy.revanced.tiktok.misc.login.FixGoogleLogin
+import io.github.nexalloy.revanced.tiktok.misc.share.SanitizeShareUrls
 
 val TikTokPatches = arrayOf(
     HideAds,
@@ -14,4 +21,11 @@ val TikTokPatches = arrayOf(
     EnableDownloads,
     DisableLoginRequirement,
     FixGoogleLogin,
+    SanitizeShareUrls,
+    OpenExternalLinks,
+    StopVideoLooping,
+    HideQuickCommentReactions,
+    DisableLongPressQuickShare,
+    DisableLongPressRepost,
+    DisableCommonAnalytics,
 )
