7fc7a501475bd87509e1e1b7c9ac2574a55bdabf
Alaa <alahus2591@gmail.com>
2026-09-22 09:02:42 +0200

    style: group social privacy imports

--- app/src/main/java/io/github/nexalloy/revanced/messenger/MessengerHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/messenger/MessengerHook.kt
@@ -2,7 +2,6 @@ package io.github.nexalloy.revanced.messenger
 
 import io.github.nexalloy.revanced.messenger.ads.HideInboxAds
 import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
-
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
 val MessengerPatches = arrayOf(
