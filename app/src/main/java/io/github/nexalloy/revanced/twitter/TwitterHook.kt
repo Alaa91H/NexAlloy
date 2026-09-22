a52524f2a8b3d1f847359e943909129de73ffc3a
Alaa <alahus2591@gmail.com>
2026-09-22 08:58:41 +0200

    feat: wire expanded Reddit X and Meta patches

--- app/src/main/java/io/github/nexalloy/revanced/twitter/TwitterHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/twitter/TwitterHook.kt
@@ -1,7 +1,11 @@
 package io.github.nexalloy.revanced.twitter
 
+import io.github.nexalloy.revanced.twitter.privacy.BlockRedirectToXLite
+import io.github.nexalloy.revanced.twitter.privacy.SanitizeXSharingLinks
 import io.github.nexalloy.revanced.twitter.timeline.HidePromotedTimelineEntries
 
 val TwitterPatches = arrayOf(
     HidePromotedTimelineEntries,
+    BlockRedirectToXLite,
+    SanitizeXSharingLinks,
 )
