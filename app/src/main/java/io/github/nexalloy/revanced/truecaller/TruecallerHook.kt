01c7ea7b194f9c935f487f93e619e348bda6ca11
Alaa <alahus2591@gmail.com>
2026-09-22 03:34:47 +0200

    style: group restriction patch imports

--- app/src/main/java/io/github/nexalloy/revanced/truecaller/TruecallerHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/truecaller/TruecallerHook.kt
@@ -6,7 +6,6 @@ import io.github.nexalloy.revanced.truecaller.misc.DisableAppStartTelemetry
 import io.github.nexalloy.revanced.truecaller.misc.DisableUpdateCheck
 import io.github.nexalloy.revanced.truecaller.premium.HideUpgradePrompts
 import io.github.nexalloy.revanced.truecaller.premium.RemovePremiumUi
-
 import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val TruecallerPatches = arrayOf(
