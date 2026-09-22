54e65e0a5c7f6c17248f8427e372a59844eae179
Alaa <alahus2591@gmail.com>
2026-09-22 03:34:42 +0200

    style: group restriction patch imports

--- app/src/main/java/io/github/nexalloy/revanced/protonvpn/ProtonVpnHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/protonvpn/ProtonVpnHook.kt
@@ -2,7 +2,6 @@ package io.github.nexalloy.revanced.protonvpn
 
 import io.github.nexalloy.revanced.protonvpn.delay.RemoveServerChangeDelay
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
-
 import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val ProtonVpnPatches = arrayOf(
