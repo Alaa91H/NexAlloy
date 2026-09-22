aac719c65568486f668a989c283814648e686b3e
Alaa <alahus2591@gmail.com>
2026-09-22 08:49:23 +0200

    feat: expose screen capture bypass for existing apps

--- app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
@@ -1,5 +1,9 @@
 package io.github.nexalloy.revanced.meta
 
 import io.github.nexalloy.revanced.meta.ads.HideAds
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
-val MetaPatches = arrayOf(HideAds)
+val MetaPatches = arrayOf(
+    AllowScreenCapture,
+    HideAds,
+)
