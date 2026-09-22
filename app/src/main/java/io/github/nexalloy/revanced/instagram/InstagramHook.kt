36d89e979e788514e60047af12a9ebde297c9fcf
Alaa <alahus2591@gmail.com>
2026-09-22 08:58:45 +0200

    feat: wire expanded Reddit X and Meta patches

--- app/src/main/java/io/github/nexalloy/revanced/instagram/InstagramHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/instagram/InstagramHook.kt
@@ -1,9 +1,15 @@
 package io.github.nexalloy.revanced.instagram
 
 import io.github.nexalloy.revanced.instagram.download.EnableNativeDownloads
+import io.github.nexalloy.revanced.instagram.privacy.DisableScreenshotDetection
+import io.github.nexalloy.revanced.instagram.stories.DisableStoryFlipping
 import io.github.nexalloy.revanced.meta.ads.HideAds
+import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
 
 val InstagramPatches = arrayOf(
     HideAds,
     EnableNativeDownloads,
+    DisableScreenshotDetection,
+    DisableStoryFlipping,
+    SanitizeMetaSharingLinks,
 )
