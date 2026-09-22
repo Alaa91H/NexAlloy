f8ab5de593ec7a4a71b4f06f27e730dfefed1c5a
Alaa <alahus2591@gmail.com>
2026-09-22 08:49:21 +0200

    feat: expose screen capture bypass for existing apps

--- app/src/main/java/io/github/nexalloy/revanced/instagram/InstagramHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/instagram/InstagramHook.kt
@@ -2,8 +2,10 @@ package io.github.nexalloy.revanced.instagram
 
 import io.github.nexalloy.revanced.instagram.download.EnableNativeDownloads
 import io.github.nexalloy.revanced.meta.ads.HideAds
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val InstagramPatches = arrayOf(
+    AllowScreenCapture,
     HideAds,
     EnableNativeDownloads,
 )
