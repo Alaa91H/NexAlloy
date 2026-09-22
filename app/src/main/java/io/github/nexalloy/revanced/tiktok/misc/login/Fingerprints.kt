f842d4423cc0ec8f22fda16437387ec7646179c6
Alaa <alahus2591@gmail.com>
2026-09-22 02:22:28 +0200

    feat: add TikTok Google auth fingerprints

--- app/src/main/java/io/github/nexalloy/revanced/tiktok/misc/login/Fingerprints.kt
+++ app/src/main/java/io/github/nexalloy/revanced/tiktok/misc/login/Fingerprints.kt
@@ -1,5 +1,6 @@
 package io.github.nexalloy.revanced.tiktok.misc.login
 
+import io.github.nexalloy.morphe.AccessFlags
 import io.github.nexalloy.morphe.fingerprint
 import org.luckypray.dexkit.query.enums.StringMatchType
 
@@ -11,4 +12,18 @@ val enableForcedLoginFingerprint = fingerprint {
 val shouldShowForcedLoginFingerprint = fingerprint {
     methodMatcher { name = "shouldShowForcedLogin" }
     classMatcher { className(".MandatoryLoginService", StringMatchType.EndsWith) }
+}
+
+val googleAuthAvailableFingerprint = fingerprint {
+    returns("Z")
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    parameters()
+    classMatcher { descriptor = "Lcom/bytedance/lobby/google/GoogleAuth;" }
+}
+
+val googleOneTapAuthAvailableFingerprint = fingerprint {
+    returns("Z")
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    parameters()
+    classMatcher { descriptor = "Lcom/bytedance/lobby/google/GoogleOneTapAuth;" }
 }
