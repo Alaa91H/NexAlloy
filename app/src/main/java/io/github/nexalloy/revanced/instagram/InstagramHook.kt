5545ba1d5b3f851a2cb77dae2d6ed612f1bde407
Alaa <alahus2591@gmail.com>
2026-09-22 09:02:30 +0200

    style: group social privacy imports

--- app/src/main/java/io/github/nexalloy/revanced/instagram/InstagramHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/instagram/InstagramHook.kt
@@ -6,7 +6,6 @@ import io.github.nexalloy.revanced.instagram.privacy.DisableScreenshotDetection
 import io.github.nexalloy.revanced.instagram.stories.DisableStoryFlipping
 import io.github.nexalloy.revanced.meta.ads.HideAds
 import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
-
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
 val InstagramPatches = arrayOf(
