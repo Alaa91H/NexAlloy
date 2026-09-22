0bae18ae07ad50361774dd5e84328e447be40d49
Alaa <alahus2591@gmail.com>
2026-09-22 09:08:05 +0200

    feat: wire Twitch and TikTok expansion patches

--- app/src/main/java/io/github/nexalloy/revanced/tiktok/TikTokHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/tiktok/TikTokHook.kt
@@ -1,17 +1,21 @@
 package io.github.nexalloy.revanced.tiktok
 
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 import io.github.nexalloy.revanced.tiktok.ad.HideAds
 import io.github.nexalloy.revanced.tiktok.interaction.downloads.EnableDownloads
 import io.github.nexalloy.revanced.tiktok.interaction.seekbar.ShowSeekbar
 import io.github.nexalloy.revanced.tiktok.interaction.speed.EnablePlaybackSpeed
 import io.github.nexalloy.revanced.tiktok.misc.login.DisableLoginRequirement
 import io.github.nexalloy.revanced.tiktok.misc.login.FixGoogleLogin
+import io.github.nexalloy.revanced.tiktok.privacy.SanitizeTikTokSharingLinks
 
 val TikTokPatches = arrayOf(
+    DisableCommonAnalytics,
     HideAds,
     ShowSeekbar,
     EnablePlaybackSpeed,
     EnableDownloads,
     DisableLoginRequirement,
     FixGoogleLogin,
+    SanitizeTikTokSharingLinks,
 )
