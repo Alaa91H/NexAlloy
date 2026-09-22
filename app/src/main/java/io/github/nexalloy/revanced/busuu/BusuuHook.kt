4bee2dc268d24cf2be94bb6e51e0be281b658440
Alaa <alahus2591@gmail.com>
2026-09-22 03:34:33 +0200

    style: group restriction patch imports

--- app/src/main/java/io/github/nexalloy/revanced/busuu/BusuuHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/busuu/BusuuHook.kt
@@ -2,7 +2,6 @@ package io.github.nexalloy.revanced.busuu
 
 import io.github.nexalloy.revanced.shared.privacy.BlockCommonDisplayAds
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
-
 import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val BusuuPatches = arrayOf(
