8d5d482ff3f6ce445b5b0fd3956a56b1c71d8dce
Alaa <alahus2591@gmail.com>
2026-09-22 09:24:46 +0200

    feat(instagram): register privacy link and story patches

--- app/src/main/java/io/github/nexalloy/revanced/instagram/InstagramHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/instagram/InstagramHook.kt
@@ -1,9 +1,15 @@
 package io.github.nexalloy.revanced.instagram
 
 import io.github.nexalloy.revanced.instagram.download.EnableNativeDownloads
+import io.github.nexalloy.revanced.instagram.links.SanitizeShareLinks
+import io.github.nexalloy.revanced.instagram.privacy.DisableScreenshotDetection
+import io.github.nexalloy.revanced.instagram.stories.DisableStoryFlipping
 import io.github.nexalloy.revanced.meta.ads.HideAds
 
 val InstagramPatches = arrayOf(
     HideAds,
     EnableNativeDownloads,
+    SanitizeShareLinks,
+    DisableScreenshotDetection,
+    DisableStoryFlipping,
 )
