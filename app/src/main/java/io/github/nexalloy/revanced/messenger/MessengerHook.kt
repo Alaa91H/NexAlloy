446ef5d498e4cb60e24c5a8efe4dad2c5100cbf4
Alaa <alahus2591@gmail.com>
2026-09-22 09:35:11 +0200

    feat: expand privacy and ad controls for existing apps

--- app/src/main/java/io/github/nexalloy/revanced/messenger/MessengerHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/messenger/MessengerHook.kt
@@ -1,7 +1,11 @@
 package io.github.nexalloy.revanced.messenger
 
 import io.github.nexalloy.revanced.messenger.ads.HideInboxAds
+import io.github.nexalloy.revanced.shared.privacy.BlockCommonDisplayAds
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
 val MessengerPatches = arrayOf(
     HideInboxAds,
+    DisableCommonAnalytics,
+    BlockCommonDisplayAds,
 )
