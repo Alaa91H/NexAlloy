33294a06c32532e9cc2917a5547dade4dbad15b3
Alaa <alahus2591@gmail.com>
2026-09-22 02:47:02 +0200

    fix: import Member hook extension

--- app/src/main/java/io/github/nexalloy/revanced/truecaller/analytics/DisableAnalyticsPatch.kt
+++ app/src/main/java/io/github/nexalloy/revanced/truecaller/analytics/DisableAnalyticsPatch.kt
@@ -1,6 +1,7 @@
 package io.github.nexalloy.revanced.truecaller.analytics
 
 import de.robv.android.xposed.XC_MethodReplacement
+import io.github.nexalloy.hookMethod
 import io.github.nexalloy.patch
 
 val DisableAnalytics = patch(
