ccb72be1ba75528142514b41441dcfe4d5f6aa27
Alaa <alahus2591@gmail.com>
2026-09-22 09:02:36 +0200

    style: group social privacy imports

--- app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/meta/MetaHook.kt
@@ -2,7 +2,6 @@ package io.github.nexalloy.revanced.meta
 
 import io.github.nexalloy.revanced.meta.ads.HideAds
 import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
-
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
 val MetaPatches = arrayOf(
