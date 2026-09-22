06572986f3447defb8b48518c5ea7a6aedd4ed2f
Alaa <alahus2591@gmail.com>
2026-09-22 08:58:38 +0200

    feat: wire expanded Reddit X and Meta patches

--- app/src/main/java/io/github/nexalloy/morphe/reddit/RedditHook.kt
+++ app/src/main/java/io/github/nexalloy/morphe/reddit/RedditHook.kt
@@ -1,9 +1,15 @@
 package io.github.nexalloy.morphe.reddit
 
 import io.github.nexalloy.morphe.reddit.ad.HideAds
+import io.github.nexalloy.morphe.reddit.layout.subredditdialog.RemoveNsfwWarning
+import io.github.nexalloy.morphe.reddit.misc.openlink.OpenLinksDirectly
+import io.github.nexalloy.morphe.reddit.misc.openlink.OpenLinksExternally
 import io.github.nexalloy.morphe.reddit.misc.privacy.SanitizeSharingLinks
 
 val RedditPatches = arrayOf(
     HideAds,
     SanitizeSharingLinks,
+    OpenLinksDirectly,
+    OpenLinksExternally,
+    RemoveNsfwWarning,
 )
