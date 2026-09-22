161e3651c50bc71795762360ee44357e9b9194a5
Alaa <alahus2591@gmail.com>
2026-09-22 03:33:52 +0200

    feat: allow optional screen capture restriction bypass

--- app/src/main/java/io/github/nexalloy/revanced/swiftbackup/SwiftBackupHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/swiftbackup/SwiftBackupHook.kt
@@ -2,6 +2,9 @@ package io.github.nexalloy.revanced.swiftbackup
 
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
+
 val SwiftBackupPatches = arrayOf(
+    AllowScreenCapture,
     DisableCommonAnalytics,
 )
