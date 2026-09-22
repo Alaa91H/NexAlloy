3906cabec24ebe829cffbaecb7fcce6da8fbb7bb
Alaa <alahus2591@gmail.com>
2026-09-22 02:39:46 +0200

    feat: add truecaller ads runtime patch

---
+++ app/src/main/java/io/github/nexalloy/revanced/truecaller/ads/HideAdsPatch.kt
@@ -0,0 +1,15 @@
+package io.github.nexalloy.revanced.truecaller.ads
+
+import de.robv.android.xposed.XC_MethodReplacement
+import io.github.nexalloy.patch
+
+val HideAds = patch(
+    name = "Hide Truecaller ads",
+    description = "Blocks after-call and Neo caller-ID ad update entry points when present.",
+) {
+    ::afterCallMaybeUpdateAdFingerprint.memberOrNull
+        ?.hookMethod(XC_MethodReplacement.DO_NOTHING)
+
+    ::neoAfterCallMaybeUpdateAdFingerprint.memberOrNull
+        ?.hookMethod(XC_MethodReplacement.DO_NOTHING)
+}
