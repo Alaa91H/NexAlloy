9d80a28a2ff7c2a186fc8407b5d49a4ab163edcc
Alaa <alahus2591@gmail.com>
2026-09-22 02:42:04 +0200

    feat: register Truecaller Premium UI cleanup

--- app/src/main/java/io/github/nexalloy/revanced/truecaller/TruecallerHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/truecaller/TruecallerHook.kt
@@ -5,11 +5,13 @@ import io.github.nexalloy.revanced.truecaller.analytics.DisableAnalytics
 import io.github.nexalloy.revanced.truecaller.misc.DisableAppStartTelemetry
 import io.github.nexalloy.revanced.truecaller.misc.DisableUpdateCheck
 import io.github.nexalloy.revanced.truecaller.premium.HideUpgradePrompts
+import io.github.nexalloy.revanced.truecaller.premium.RemovePremiumUi
 
 val TruecallerPatches = arrayOf(
     HideAds,
     DisableAnalytics,
     DisableAppStartTelemetry,
     HideUpgradePrompts,
+    RemovePremiumUi,
     DisableUpdateCheck,
 )
