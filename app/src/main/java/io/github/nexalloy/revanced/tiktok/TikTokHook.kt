29b73ea311bf407c527e4a4755739f0c0a9c1887
Alaa <alahus2591@gmail.com>
2026-09-22 02:22:30 +0200

    feat: register expanded TikTok runtime patches

--- app/src/main/java/io/github/nexalloy/revanced/tiktok/TikTokHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/tiktok/TikTokHook.kt
@@ -1,9 +1,15 @@
 package io.github.nexalloy.revanced.tiktok
 
+import io.github.nexalloy.revanced.tiktok.ad.HideAds
+import io.github.nexalloy.revanced.tiktok.interaction.downloads.EnableDownloads
 import io.github.nexalloy.revanced.tiktok.interaction.seekbar.ShowSeekbar
 import io.github.nexalloy.revanced.tiktok.misc.login.DisableLoginRequirement
+import io.github.nexalloy.revanced.tiktok.misc.login.FixGoogleLogin
 
 val TikTokPatches = arrayOf(
+    HideAds,
     ShowSeekbar,
+    EnableDownloads,
     DisableLoginRequirement,
+    FixGoogleLogin,
 )
