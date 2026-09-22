182e70301175b6180ee9084701f34b6e1411a7f0
Alaa <alahus2591@gmail.com>
2026-09-22 02:44:20 +0200

    feat: add revanced swiftbackup support

---
+++ app/src/main/java/io/github/nexalloy/revanced/swiftbackup/SwiftBackupHook.kt
@@ -0,0 +1,7 @@
+package io.github.nexalloy.revanced.swiftbackup
+
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
+
+val SwiftBackupPatches = arrayOf(
+    DisableCommonAnalytics,
+)
