29d6b9711a7ab13e194b3bc9fe39e62032eb5116
Alaa <alahus2591@gmail.com>
2026-09-22 03:34:30 +0200

    style: group restriction patch imports

--- app/src/main/java/io/github/nexalloy/revanced/duolingo/DuolingoHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/duolingo/DuolingoHook.kt
@@ -3,7 +3,6 @@ package io.github.nexalloy.revanced.duolingo
 import io.github.nexalloy.revanced.duolingo.ads.DisableAds
 import io.github.nexalloy.revanced.duolingo.icon.DisableDynamicIcon
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
-
 import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val DuolingoPatches = arrayOf(
