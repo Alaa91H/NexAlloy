565c0664d4c840e399b44de5738735d51ee5f5b9
Alaa <alahus2591@gmail.com>
2026-09-22 09:02:27 +0200

    style: group social privacy imports

--- app/src/main/java/io/github/nexalloy/revanced/twitter/TwitterHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/twitter/TwitterHook.kt
@@ -3,7 +3,6 @@ package io.github.nexalloy.revanced.twitter
 import io.github.nexalloy.revanced.twitter.privacy.BlockRedirectToXLite
 import io.github.nexalloy.revanced.twitter.privacy.SanitizeXSharingLinks
 import io.github.nexalloy.revanced.twitter.timeline.HidePromotedTimelineEntries
-
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
 val TwitterPatches = arrayOf(
