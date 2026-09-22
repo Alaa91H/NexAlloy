cd561408e83b051572dc0697944ec9e887afdad8
Alaa <alahus2591@gmail.com>
2026-09-22 08:59:06 +0200

    feat: wire expanded Reddit X and Meta patches

--- app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
@@ -1,7 +1,9 @@
 package io.github.nexalloy.revanced.facebook
 
 import io.github.nexalloy.revanced.facebook.ads.HideStoryAds
+import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
 
 val FacebookPatches = arrayOf(
     HideStoryAds,
+    SanitizeMetaSharingLinks,
 )
