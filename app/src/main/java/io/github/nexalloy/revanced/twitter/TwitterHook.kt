d566ab85be6dc0f21b44bc8a185402f37483f09b
Alaa <alahus2591@gmail.com>
2026-09-22 09:01:28 +0200

    feat: add optional common analytics suppression to social apps

--- app/src/main/java/io/github/nexalloy/revanced/twitter/TwitterHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/twitter/TwitterHook.kt
@@ -4,7 +4,10 @@ import io.github.nexalloy.revanced.twitter.privacy.BlockRedirectToXLite
 import io.github.nexalloy.revanced.twitter.privacy.SanitizeXSharingLinks
 import io.github.nexalloy.revanced.twitter.timeline.HidePromotedTimelineEntries
 
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
+
 val TwitterPatches = arrayOf(
+    DisableCommonAnalytics,
     HidePromotedTimelineEntries,
     BlockRedirectToXLite,
     SanitizeXSharingLinks,
