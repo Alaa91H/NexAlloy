6964037e01ba5610c61a7348cab32ef73caec783
Alaa <alahus2591@gmail.com>
2026-09-22 03:33:49 +0200

    feat: allow optional screen capture restriction bypass

--- app/src/main/java/io/github/nexalloy/revanced/protonvpn/ProtonVpnHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/protonvpn/ProtonVpnHook.kt
@@ -3,7 +3,10 @@ package io.github.nexalloy.revanced.protonvpn
 import io.github.nexalloy.revanced.protonvpn.delay.RemoveServerChangeDelay
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
+
 val ProtonVpnPatches = arrayOf(
+    AllowScreenCapture,
     RemoveServerChangeDelay,
     DisableCommonAnalytics,
 )
