9602473f5eeb98fed1618317156a96cdf9e5f7cb
Alaa <alahus2591@gmail.com>
2026-09-22 08:49:13 +0200

    feat: expose screen capture bypass for existing apps

--- app/src/main/java/io/github/nexalloy/morphe/reddit/RedditHook.kt
+++ app/src/main/java/io/github/nexalloy/morphe/reddit/RedditHook.kt
@@ -2,8 +2,10 @@ package io.github.nexalloy.morphe.reddit
 
 import io.github.nexalloy.morphe.reddit.ad.HideAds
 import io.github.nexalloy.morphe.reddit.misc.privacy.SanitizeSharingLinks
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val RedditPatches = arrayOf(
+    AllowScreenCapture,
     HideAds,
     SanitizeSharingLinks,
 )
