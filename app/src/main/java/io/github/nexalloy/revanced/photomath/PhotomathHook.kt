05bfe65a1b7c411058ad91b4c93f16309700c813
Alaa <alahus2591@gmail.com>
2026-09-22 08:49:19 +0200

    feat: expose screen capture bypass for existing apps

--- app/src/main/java/io/github/nexalloy/revanced/photomath/PhotomathHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/photomath/PhotomathHook.kt
@@ -1,5 +1,9 @@
 package io.github.nexalloy.revanced.photomath
 
 import io.github.nexalloy.revanced.photomath.misc.unlock.plus.UnlockPlus
+import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
-val PhotomathPatches = arrayOf(UnlockPlus)
+val PhotomathPatches = arrayOf(
+    AllowScreenCapture,
+    UnlockPlus,
+)
