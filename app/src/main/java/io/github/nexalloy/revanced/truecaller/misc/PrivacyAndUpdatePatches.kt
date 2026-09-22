20b65283fb8a7dd4a84bd1b8443e1db66e95f70c
Alaa <alahus2591@gmail.com>
2026-09-22 02:47:05 +0200

    fix: import Member hook extension

--- app/src/main/java/io/github/nexalloy/revanced/truecaller/misc/PrivacyAndUpdatePatches.kt
+++ app/src/main/java/io/github/nexalloy/revanced/truecaller/misc/PrivacyAndUpdatePatches.kt
@@ -1,6 +1,7 @@
 package io.github.nexalloy.revanced.truecaller.misc
 
 import de.robv.android.xposed.XC_MethodReplacement
+import io.github.nexalloy.hookMethod
 import io.github.nexalloy.patch
 
 val DisableAppStartTelemetry = patch(
