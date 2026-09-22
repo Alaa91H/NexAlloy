9cf8f958a3a17ce696f821ae06282d9012091a0b
Alaa <alahus2591@gmail.com>
2026-09-22 08:59:02 +0200

    feat: wire expanded Reddit X and Meta patches

--- app/src/main/java/io/github/nexalloy/revanced/messenger/MessengerHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/messenger/MessengerHook.kt
@@ -1,7 +1,9 @@
 package io.github.nexalloy.revanced.messenger
 
 import io.github.nexalloy.revanced.messenger.ads.HideInboxAds
+import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
 
 val MessengerPatches = arrayOf(
     HideInboxAds,
+    SanitizeMetaSharingLinks,
 )
