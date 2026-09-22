f72466dda7e6112515428cbeb49be8bf68eb53a7
Alaa <alahus2591@gmail.com>
2026-09-22 09:02:46 +0200

    style: group social privacy imports

--- app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
@@ -2,7 +2,6 @@ package io.github.nexalloy.revanced.facebook
 
 import io.github.nexalloy.revanced.facebook.ads.HideStoryAds
 import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
-
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
 
 val FacebookPatches = arrayOf(
