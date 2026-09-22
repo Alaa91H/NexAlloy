3024fe82d5030c00cf2c48de058ab49519b9520f
Alaa <alahus2591@gmail.com>
2026-09-22 02:39:41 +0200

    feat: add revanced truecaller runtime patch

---
+++ app/src/main/java/io/github/nexalloy/revanced/truecaller/TruecallerHook.kt
@@ -0,0 +1,15 @@
+package io.github.nexalloy.revanced.truecaller
+
+import io.github.nexalloy.revanced.truecaller.ads.HideAds
+import io.github.nexalloy.revanced.truecaller.analytics.DisableAnalytics
+import io.github.nexalloy.revanced.truecaller.misc.DisableAppStartTelemetry
+import io.github.nexalloy.revanced.truecaller.misc.DisableUpdateCheck
+import io.github.nexalloy.revanced.truecaller.premium.HideUpgradePrompts
+
+val TruecallerPatches = arrayOf(
+    HideAds,
+    DisableAnalytics,
+    DisableAppStartTelemetry,
+    HideUpgradePrompts,
+    DisableUpdateCheck,
+)
