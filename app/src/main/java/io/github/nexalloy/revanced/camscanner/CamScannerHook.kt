ab7e24af7a0611b0339a84f0619f49c54b511c44
Alaa <alahus2591@gmail.com>
2026-09-22 03:34:35 +0200

    style: group restriction patch imports

--- app/src/main/java/io/github/nexalloy/revanced/camscanner/CamScannerHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/camscanner/CamScannerHook.kt
@@ -3,7 +3,6 @@ package io.github.nexalloy.revanced.camscanner
 import io.github.nexalloy.revanced.camscanner.telemetry.DisableTelemetry
 import io.github.nexalloy.revanced.shared.privacy.BlockCommonDisplayAds
 import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
-
 import io.github.nexalloy.revanced.shared.restrictions.AllowScreenCapture
 
 val CamScannerPatches = arrayOf(
