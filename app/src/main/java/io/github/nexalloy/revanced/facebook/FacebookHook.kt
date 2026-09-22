3faaace27c17058bd6544417dc64d21ad6969a73
Alaa <alahus2591@gmail.com>
2026-09-22 09:01:47 +0200

    feat: add optional common analytics suppression to social apps

--- app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
@@ -3,7 +3,10 @@ package io.github.nexalloy.revanced.facebook
 import io.github.nexalloy.revanced.facebook.ads.HideStoryAds
 import io.github.nexalloy.revanced.meta.privacy.SanitizeMetaSharingLinks
 
+import io.github.nexalloy.revanced.shared.privacy.DisableCommonAnalytics
+
 val FacebookPatches = arrayOf(
+    DisableCommonAnalytics,
     HideStoryAds,
     SanitizeMetaSharingLinks,
 )
